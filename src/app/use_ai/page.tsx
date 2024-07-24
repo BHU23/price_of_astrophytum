0814cbdbe50328147b87e61bd53d8983180f044d
Bhuwadol <125109678+BHU23@users.noreply.github.com>
2024-07-24 15:31:02 +0700

    start project

---
+++ src/app/use_ai/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/header"
+
+export default function UseAI() {
+    return (
+        <div>
+            <Header></Header>
+        <div>Use AI</div>
+      </div>
+    );
+}
