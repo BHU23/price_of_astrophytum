19c445de26c601498cb11ad5b3fd0aad105bf068
Mixkenton <mixkenton@gmail.com>
2024-08-14 16:06:20 +0700

    Update Dashboard

---
+++ src/components/box_yourpost.tsx
@@ -0,0 +1,16 @@
+interface BoxPostInt {
+  img: any;
+}
+
+export default function BoxPost({ img }: BoxPostInt) {
+  return (
+    <div className="flex flex-col bg-pear rounded-md mr-5 mb-5 w-auto ">
+      <div className="flex justify-center items-center">
+        <img className="rounded-md object-contain " src={img} />
+      </div>
+      <div className="text-black font-bold flex justify-center m-1 cursor-pointer ">
+        เลือก
+      </div>
+    </div>
+  );
+}
