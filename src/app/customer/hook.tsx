ca0f3a982d334e38939cbcd2310bb26e7643a630
Bhuwadol <125109678+BHU23@users.noreply.github.com>
2024-08-26 11:43:18 +0700

    update header

--- src/app/customer/hook.tsx
+++ src/app/customer/hook.tsx
@@ -1,19 +1,16 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import { useState } from "react";
-import generateBreadcrumbLinks from "./hook/breadcrum.hook";
 import { useGlobal } from "@/context/useGoble";
 
 export default function useCustumer() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const { isOpen, setIsOpen } = useGlobal();
+  const { isOpen, setIsOpen, breadcrumbLinks } = useGlobal();
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
     setIsOpen(false);
   };
-  const path = usePathname();
-  const breadcrumbLinks = generateBreadcrumbLinks(path);
+  
 
   return {
     custumerItems: {
